c598c1d9401796b8369ae71c5c1bab7274589992
bevankoopman <bevan@koopman.id.au>
2014-09-02 14:28:28 +1000

    Calc. true negatives

--- evaluator.py
+++ evaluator.py
@@ -20,7 +20,7 @@ class ConfusionMatrix():
 		self.classification = classification
 		self.true_positive = 0
 		self.false_positive = 0
-		self.true_negative = 0
+		#self.true_negative = -1
 		self.false_negative = 0
 
 	def increment_true_positive(self):
@@ -68,14 +68,46 @@ class ConfusionMatrix():
 		print "\t%s Precsion: %.4f" % (self.classification, self.precision())
 		print "\t%s Fmeasure: %.4f" % (self.classification, self.fmeasure())
 
-def get_binary_predictions(binary_prediction_file):
+def get_binary_predictions(binary_prediction_file, weka=False):
 	binary = {}
-	for line in open(binary_prediction_file):
-		items = line.split()
-		docId = items[0]
-		cancer = items[1] if len(items) > 1 else "0"
-		binary[docId] = cancer
+	if weka:
+		start_prediction = False
+		for line in open(binary_prediction_file):
+			if start_prediction:
+				items = line.split()
+				if len(items) == 4:
+					docid, coding = items[0:2]
+					binary[docid.replace(".json", "")] = coding if coding == "cancer" else "0"
+			if line.startswith("==Predictions=="):
+				start_prediction = True
+	else:
+		for line in open(binary_prediction_file):
+			items = line.split()
+			docId = items[0]
+			cancer = items[1] if len(items) > 1 else "0"
+			binary[docId] = cancer
+	
 	return binary
+
+
+def read_predictions(prediction_file, icd, weka):
+	with open(prediction_file) as fh:
+		predictions = False
+		for line in fh:
+			if weka:
+				if predictions and not line.startswith("Total"):
+					docId, pred, prob_prid, prob_not = line.split()
+					predictions = [pred]
+					yield docId.replace(".json", ""), predictions
+				if line.startswith("==Predictions=="):
+					predictions = True
+			else:
+				items = line.split()
+				docId = items[0]
+				predictions = [p.strip() for p in items[1:]]
+
+				yield docId, predictions
+
 
 if __name__ == '__main__':
 
@@ -83,56 +115,83 @@ if __name__ == '__main__':
 	argparser.add_argument("-p", "--prediction_file", required=True, help="Predictions file.")
 	argparser.add_argument("-g", "--ground_truth_file", required=True, help="Ground truth file.")
 	argparser.add_argument("-b", "--binary_prediction_file", required=False, help="Binary prediction to filter the prediction_file.")
+	argparser.add_argument("-bw", "--binary_weka", action='store_true', default=False, help="Binary prediction is in Weka format.")
 	argparser.add_argument('--icd', action='store_true', default=False, help="Whether the evaluation is using ICD code; in which case consider only first three characters (CXX). Default is False")
+	argparser.add_argument("-t", '--tex', action='store_true', default=False, help="Print results in LaTeX table format.")
+	argparser.add_argument("-w", '--weka', action='store_true', default=False, help="Predictions are in weka format")
 
 	prediction_file = argparser.parse_args().prediction_file
 	ground_truth_file = argparser.parse_args().ground_truth_file
 	binary_prediction_file = argparser.parse_args().binary_prediction_file
 	icd = argparser.parse_args().icd
+	latex = argparser.parse_args().tex
+	weka = argparser.parse_args().weka
+	binary_weka = argparser.parse_args().binary_weka
 
 	gt = read_ground_truth(ground_truth_file, icd)
 	binary = {}
 	if binary_prediction_file:
-		binary = get_binary_predictions(binary_prediction_file)
+		binary = get_binary_predictions(binary_prediction_file, binary_weka)
 	
 	confusion_matices = {}
 
 	print "docId\tActual\tPredictions (1..n) *=correct"
-	with open(prediction_file) as fh:
-		for line in fh:
-			items = line.split()
-			docId = items[0]
-			predictions = [p.strip() for p in items[1:]]
+	pred_count = 0
+	for docId, predictions in read_predictions(prediction_file, icd, weka):
+		pred_count = pred_count + 1
 
-			if len(binary) > 0 and binary[docId] == "0":
-				continue
+		if len(binary) > 0 and binary[docId] == "0":
+			continue
 
-			conf_mat = confusion_matices.get(gt[docId], ConfusionMatrix(gt[docId]))
+		conf_mat = confusion_matices.get(gt[docId], ConfusionMatrix(gt[docId]))
 
-			
-			correct = False
+		correct = False
+		for prediction in predictions:
+			if icd:
+				prediction = prediction[0:3]	
+			if prediction == gt[docId]:
+				correct = True
+				conf_mat.increment_true_positive()
+				break
+		confusion_matices[gt[docId]] = conf_mat
+
+		labeled_predictions = [p+('*' if p == gt[docId] else "") for p in predictions]
+
+		print "%s\t%s\t%s" % (docId, gt[docId], reduce(lambda x,y: x+"\t"+y, labeled_predictions))
+
+		if not correct:
 			for prediction in predictions:
-				if icd:
-					prediction = prediction[0:3]	
-				if prediction == gt[docId]:
-					correct = True
-					conf_mat.increment_true_positive()
-					break
-			confusion_matices[gt[docId]] = conf_mat
-
-			labeled_predictions = [p+('*' if p == gt[docId] else "") for p in predictions]
-
-			print "%s\t%s\t%s" % (docId, gt[docId], reduce(lambda x,y: x+"\t"+y, labeled_predictions))
-
-			if not correct:
-				for prediction in predictions:
-					conf_mat.increment_false_negative()
-					other_conf_matrix = confusion_matices.get(prediction, ConfusionMatrix(prediction))
-					other_conf_matrix.increment_false_positive()
-					confusion_matices[prediction] = other_conf_matrix
+				conf_mat.increment_false_negative()
+				other_conf_matrix = confusion_matices.get(prediction, ConfusionMatrix(prediction))
+				other_conf_matrix.increment_false_positive()
+				confusion_matices[prediction] = other_conf_matrix
 	
 	print "\n== SUMMARY ==\n"
 	for conf_mat in confusion_matices.values():
+		conf_mat.true_negative = pred_count - conf_mat.true_positive - conf_mat.false_positive - conf_mat.false_negative
 		print conf_mat
 		conf_mat.summary_measures()
 		print ""
+		print conf_mat.classification, conf_mat.precision(), conf_mat.recall(), conf_mat.fmeasure(), conf_mat.true_positive, conf_mat.false_positive
+
+	if latex:
+		print '''
+\\begin{table}
+ \\small
+ \\centering
+ \\begin{tabular}{lrrr|rrcl}
+ \\toprule	
+ \\bf Disease &  \\bf Precision & \\bf Recall & \\bf F-measure & \multicolumn{4}{c}{\\bf Confusion Matrix} \\\\
+ & & & & \multicolumn{2}{r}{Classifier} & \multicolumn{2}{r}{Ground truth} \\\\
+ & & & & - & +  \\\\
+ \\midrule '''
+		for conf_mat in confusion_matices.values():
+			print " %s & %.4f & %.4f & %.4f & %d & %d & - \\\\" % (conf_mat.classification, conf_mat.precision(), conf_mat.recall(), conf_mat.fmeasure(), conf_mat.true_negative, conf_mat.false_positive)
+			print " & & & & %d & %d & + & %s \\\\" % (conf_mat.false_negative, conf_mat.true_positive, conf_mat.classification)
+		print '''\\bottomrule
+ \\end{tabular}
+ \\caption{Classification performation results for Influenza, Diabetes and Pneumonia.}
+ \\label{tbl:disease_results}
+\\end{table}
+'''
+
